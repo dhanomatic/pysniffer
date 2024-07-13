79e382a5a91a1b9f827fad0d7726dbdee568f6fb
Dhananjayan <mail.dhananjayharidas@gmail.com>
2024-07-14 00:15:54 +0530

    Tag updated

--- src/pysniffer/__init__.py
+++ src/pysniffer/__init__.py
@@ -1,6 +1,6 @@
 # pysniffer/__init__.py
 """
-PySniffer 
+PySniffer
 
 A Python package for profiling and measuring function performance, providing detailed insights into execution time, CPU time, and peak memory usage. Ideal for developers looking to optimize their code efficiently.
 
@@ -8,4 +8,4 @@ A Python package for profiling and measuring function performance, providing det
 
 from .decorator import benchmark
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
